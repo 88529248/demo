7e1b487a6125e1e21b98d41ed3067a0addd5c34a
88529248 <88529248@qq.com>
2020-11-16 11:11:48 +0800

    commit

--- demo/src/main/java/com/example/demo/collection/MyMap.java
+++ demo/src/main/java/com/example/demo/collection/MyMap.java
@@ -10,8 +10,23 @@ public class MyMap {
 	 * 
 	 * ConcurrentHashMap K,V都不可为null
 	 */
+
+	/**
+	 * 使用：在局部方法或者线程绝对安全的情况下使用
+	 */
 	@Test
 	public void testHashMap() {
+
+	}
+
+	/**
+	 * JDK1.8和之前区别
+	 * 1. 取消分段锁机制，进一步降低冲突概率;
+	 * 2. 引入红黑树（当链表>8 && table>= 64）
+	 * 3. 增加计算Map中元素数量的方法， mappingcount()
+	 */
+	@Test
+	public void testConcurrentHashMap() {
 
 	}
 
