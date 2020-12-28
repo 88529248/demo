2d13937f809e26c611d98a504531f7895c6ed4cf
88529248 <88529248@qq.com>
2020-12-28 15:33:29 +0800

    commit

---
+++ demo/src/main/java/com/example/demo/boot/MySpring.java
@@ -0,0 +1,11 @@
+package com.example.demo.boot;
+
+public interface MySpring {
+    /**
+     * IOC:控制反转
+     * 实现原理： 1、解析xml文件（xmlBean注入方式）
+     *           2、反射获取Clazz
+     *           3、对象工厂生成Bean
+     */
+
+}
